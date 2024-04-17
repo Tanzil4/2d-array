d979b4e432ef95f0c2571a14dca17ce1b49139b0
Tanzil-khan <155762084+Tanzil4@users.noreply.github.com>
2024-04-18 00:45:42 +0500

    Add files via upload

---
+++ array print/app.js
@@ -0,0 +1,10 @@
+let myArr = [[2, 3, 4, 4, 4, 4], [5, 5], [5], [9, 8, 7]]; // 2D array
+
+for (let t = 0; t < 3; t++) {
+
+
+  for (let a = 0; a < 4; a++) {
+    console.log(`t : ${t}  a : ${a}`);
+    console.log(myArr[t][a]);
+}
+}
